f80a86207125c78fe782c7fba44164097e6321d4
Fibonacho <Fibonacho@users.noreply.github.com>
2015-11-06 22:12:21 +0100

    Update ConcurrentTransactions.java

--- ConcurrentTransactions.java
+++ ConcurrentTransactions.java
@@ -8,19 +8,20 @@
  * Lecturer: Nikolaus Augsten
  */
 
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 /** 
- * Dummy transaction that prints a start message, waits for a random time 
- * (up to 100ms) and finally prints a status message at termination.
+ * Transaction
  */
 class Transaction extends Thread {
 
     // identifier of the transaction
     int id, mode, qry;
     DBConnect db;
-                                                                                                                     
+
     Transaction(int id, DBConnect db, int qry, int mode) {
         this.id   = id;
         this.db   = db;
@@ -30,48 +31,49 @@ class Transaction extends Thread {
     
     @Override
     public void run() {
-        System.out.println("transaction " + id + " started");
         
-        // replace this with a transaction
-        int ms = (int)(Math.random()*100);
+        System.out.println("transaction " + id + " started");
+        int ms = 1000;
+
         try {
             String query = "";
-            // QUERY 1
-            if (this.qry == 1) {
+            String tableName = "flight_seats";
+
+            if (this.qry == 1) { // QUERY 1
                 System.out.println("\nQUERY " + id);
                 
-                String e = "SELECT balance FROM accounts WHERE account = " + id + ";";
-                //System.out.println("e <- " + e);                
-                e = db.executeQuery(e);
-                //System.out.println("e = " + e);
+                // 1. Retrieve list of available seats
+                query = "select id from " + tableName + " where availability = 1";
+                ArrayList<String> availSeats = new ArrayList<String>();
                 
-                query = "UPDATE accounts SET balance = " + e + " + 1 WHERE account = " + id + ";";
+                if (mode == 0)
+                    availSeats = db.retrieveSeatsReadCommitted(query);
+                else if (mode == 1)
+                    availSeats = db.retrieveSeatsSerializable(query);
+                
+                System.out.println("Number of available seats: " + availSeats.size());
+
+                // 2. Give the customer some time (decision time is 1 second) to decide on a seat
+                sleep(ms);
+                Random r        = new Random();
+                int low         = 0;                 // lower bound for random index
+                int high        = availSeats.size(); // upper bound for random index
+                int randomIndex = r.nextInt(high-low) + low; // compute random index
+
+                String randomSeat = availSeats.get(randomIndex); // select (random) seat number
+                System.out.println("SEAT: " + randomSeat);
+
+                // 3. Secure a seat (update the availability of the chosen seat to false).
+                query = "update " + tableName + " set availability = 0 where id = " + randomSeat;
+                
                 if (mode == 0)
                     db.sendReadCommitted(query);
                 else if (mode == 1)
                     db.sendSerializable(query);
-                
-                String c = "SELECT balance FROM accounts WHERE account = 0;";
-                //System.out.println("c <- " + c);
-                c = db.executeQuery(c);
-                //System.out.println("c = " + c);
-                
-                query = "UPDATE accounts SET balance = " + c + " - 1 WHERE account = 0;";
-                if (mode == 0)
-                    db.sendReadCommitted(query);
-                else if (mode == 1)
-                    db.sendSerializable(query);            
-            // QUERY 2
-            } else if (this.qry == 2) {
-                query = "UPDATE accounts SET balance = balance + 1 WHERE account = " + id + ";\n"
-                        + "UPDATE accounts SET balance = balance - 1 WHERE account = 0;";
-                
-                if (mode == 0)
-                    db.sendReadCommitted(query);
-                else if (mode == 1)
-                    db.sendSerializable(query);
-            }            
-            sleep(ms);
+
+            } else if (this.qry == 2) { // QUERY 2 TODO
+                System.out.println("\nQUERY " + id);
+            }        
         } catch (Exception e) {};
         
         System.out.println("transaction " + id + " terminated");
@@ -79,12 +81,8 @@ class Transaction extends Thread {
 }
 
 /**
- * <p>
- * Run numThreads transactions, where at most maxConcurrent transactions 
- * can run in parallel.
- * 
- * <p>params: numThreads maxConcurrent
- *
+ * Run numThreads transactions, where at most maxConcurrent transactions can run in parallel.
+ * params: numThreads maxConcurrent
  */
 public class ConcurrentTransactions {
 
@@ -101,30 +99,39 @@ public class ConcurrentTransactions {
         }
         
         DBConnect db = new DBConnect();
+        String tableName = "flight_seats";
+        db.dropTable(tableName);
+        db.createTableScheme();
+        db.fillTable();
+        
         long begin = System.currentTimeMillis();
         
         /** There are 200 customers trying to book a seat. 
          *  Each customer books a seat in a separate transaction.
          *  Imitate multiple travel agents that book the seats for the customers. 
-         *  The number of travel agents is a parameter k, where k in {1,2,4,6,8,10}. 
-         */      
-        int numThreads = 200;
-        int maxConcurrent = Integer.parseInt(args[1]);
+         *  Perform the experiment for each k. k is the number of threads performing 
+         *  the booking transactions. One thread makes one reservation at a time.
+         */
+        int numThreads      = 200; 
+        // The number of travel agents is a parameter k, where k in {1, 2, 4, 6, 8, 10}
         int[] kTravelAgents = {1, 2, 4, 6, 8, 10};
+        int maxConcurrent   = kTravelAgents[3];
 
         // create numThreads transactions
         Transaction[] trans = new Transaction[numThreads];
         
         for (int i = 0; i < trans.length; i++) {
             // mode: 0 - read committed, 1 - serializable
-            int mode = 1;
+            int mode = 0;
             int qry  = 1;
             trans[i] = new Transaction(i + 1, db, qry, mode);
         }
         
         // start all transactions using a thread pool 
-        ExecutorService pool = Executors.newFixedThreadPool(maxConcurrent);                
+        ExecutorService pool = Executors.newFixedThreadPool(maxConcurrent);
+        
         for (int i = 0; i < trans.length; i++) {
+            // ececute transaction i
             pool.execute(trans[i]);
         }
         pool.shutdown(); // end program after all transactions are done
@@ -141,16 +148,25 @@ public class ConcurrentTransactions {
 }
 
 /** Perform the following experiment:
-    - There are 200 customers trying to book a seat. Each customer books a seat in a separate transaction.
-    - Imitate multiple travel agents that book the seats for the customers. The number of travel agents is a parameter k, where k in {1,2,4,6,8,10}. Perform the experiment for each k. k is the number of threads performing the booking transactions. One thread makes one reservation at a time.
-    - Evaluate two versions of the booking transaction : a) as a single transaction including all three steps, and b) split into two smaller transactions: b1) retrieving available seats, b2) securing a seat. In case b), the decision time is not part of transactions but has to be considered.
-    - Perform the experiment for two isolation levels: 'read committed' and 'serializable'.
-    - Set explicitly row locking. If this is not possible, describe the locking mechanism.
-    - Restart the transactions until they commit (all customers book a seat).
-    
-    Measure the following properties:
-    - Total time required for all customers to book a seat (for each k, isolation level, and transaction version). Show the results on a line plot where x axis shows parameter k and y axis shows time.
-    - Min, max, and avg number of times a customer had to try to book a seat until he got it (number of times a single transaction had to be restarted). Show the results in a table.
-    
-    Briefly describe the outcome and explain the differences between the transaction versions and isolation levels.
+    - There are 200 customers trying to book a seat. 
+      Each customer books a seat in a separate transaction.
+    - Imitate multiple travel agents that book the seats for the customers. 
+      The number of travel agents is a parameter k, where k in 
+      {1,2,4,6,8,10}. 
+      Perform the experiment for each k. k is the number of threads 
+      performing the booking transactions. One thread makes one 
+      reservation at a time.
+    - Evaluate two versions of the booking transaction : 
+      a) as a single transaction including all three steps, and 
+      b) split into two smaller transactions: 
+         b1) retrieving available seats, 
+         b2) securing a seat. In case b), 
+         the decision time is not part of transactions but has to be 
+         considered.
+    - Perform the experiment for two isolation levels: 
+      'read committed' and 'serializable'.
+    - Set explicitly row locking. 
+      If this is not possible, describe the locking mechanism.
+    - Restart the transactions until they commit (all customers 
+      book a seat).
  */
